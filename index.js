097460aaa173bc031fcad88690df4b72ae6f06f1
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-05-02 15:31:32 +0900

    app responsed

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const port = process.env.PORT || 5000;
 
 // const chefs = require('./data/chefs.json');
 
-// app.get('/', (req,res) => {
-//     res.send('chef recipe is running ')
-// });
+app.get('/', (req,res) => {
+    res.send('chef recipe is running ')
+});
 
 // app.get('/chefs', (req, res) => {
 //     res.send(chefs);
 // })
 
-// app.listen(port, () => {
-//     console.log(`chef recipe is running on port ${port}`)
-// })
+app.listen(port, () => {
+    console.log(`chef recipe is running on port ${port}`)
+})
