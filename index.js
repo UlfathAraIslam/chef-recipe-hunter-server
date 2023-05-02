97b9bfe554eabd6f56d97c58a51e2647187e3615
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-05-02 15:35:04 +0900

    get chefs data

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const chefs = require('./data/chefs.json');
+const chefs = require('./data/chefs.json');
 
 app.get('/', (req,res) => {
     res.send('chef recipe is running ')
 });
 
-// app.get('/chefs', (req, res) => {
-//     res.send(chefs);
-// })
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
 app.listen(port, () => {
     console.log(`chef recipe is running on port ${port}`)
